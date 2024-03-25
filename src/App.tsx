c217fbe46d2df6237a0278bf23458a20d7a9d9ce
huangxj <2063818987@qq.com>
2024-03-25 23:31:26 +0800

    添加动态路由的示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { RouteSectionProps } from "@solidjs/router";
+import { A, RouteSectionProps } from "@solidjs/router";
 import type { Component } from "solid-js";
 
 const App: Component<RouteSectionProps> = (props) => {
-  return <div class="w-full h-full">{props.children}</div>;
+  return (
+    <div class="w-full h-full">
+      app
+      {props.children}
+      <A href="/button-example-abc">跳转</A>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,62 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
 import { Route, Router } from "@solidjs/router";
+import { For, JSX } from "solid-js";
+import { createStore } from "solid-js/store";
 import App from "./App";
+import "./index.css";
 import { ButtonExample } from "./pages/ButtonExample";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <Router root={App}>
-  <Route path={"/button-example"} component={ButtonExample} />
-</Router>, root!);
+const Main = () => {
+  // 可以通过这样实现动态添加或删除路由
+  const [routes, setRoutes] = createStore<RouterTable[]>([
+    { path: "/button-example", component: ButtonExample },
+    { path: "/button-example223", component: ButtonExample },
+  ]);
+  // const [getRoutes, setRoutes] = createSignal<RouterTable[]>([
+  //   { path: "/button-example", component: ButtonExample },
+  //   { path: "/button-example223", component: ButtonExample },
+  // ]);
+
+  setTimeout(() => {
+    setRoutes(
+      (route, i) => {
+        console.log("do");
+        return route.path === "/button-example";
+      },
+      "path",
+      "/button-example-abc"
+    );
+  }, 5_000);
+
+  return (
+    <div>
+      hello
+      <div>
+        <Router root={App}>
+          <For each={routes}>
+            {(item, getI) => {
+              console.log("rerender", item.path);
+              return <Route path={item.path} component={item.component} />;
+            }}
+          </For>
+        </Router>
+      </div>
+    </div>
+  );
+};
+render(Main, root!);
+
+interface RouterTable {
+  path: string;
+  component: (props: any) => JSX.Element;
+}
