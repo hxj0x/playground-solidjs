d27dfcec08636d34d008bde5efeff696c82196cb
huangxj <2063818987@qq.com>
2024-03-25 20:53:49 +0800

    添加vscode配置文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { RouteSectionProps } from "@solidjs/router";
+import type { Component } from "solid-js";
 
 const App: Component<RouteSectionProps> = (props) => {
-    // console.log(props);
-    return (
-        <div class="w-full h-full">
-            {props.children}
-        </div>
-
-
-    );
+  return <div class="w-full h-full">{props.children}</div>;
 };
-
-import type { Component } from 'solid-js';
 
 export default App;
