1dee81bfc40569d52c69083f4c766aa57b128f91
huangxj <2063818987@qq.com>
2024-03-30 23:46:46 +0800

    layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,11 +2,13 @@ export const Layout = () => {
   return (
     <>
       <div class="flex h-screen flex-col">
-        <header class="border-b border-slate-900/15  flex-shrink-0 h-10 py-2 px-6 flex items-center">
-          <h1 class="font-bold text-lg">后台管理系统</h1>
+        <header class="border-b border-slate-900/15  flex-shrink-0 h-10 flex items-center shadow-sm">
+          <div class="font-bold text-lg w-64 flex items-center justify-center border-r border-slate-900/15 h-full">
+            <h1>后台管理系统</h1>
+          </div>
         </header>
         <div class="flex-1 flex overflow-hidden">
-          <nav class="border-r border-slate-900/15 w-64 overflow-y-auto"></nav>
+          <nav class="border-r border-slate-900/15 w-64 overflow-y-auto shadow-lg"></nav>
           <main class="flex-1 overflow-y-auto">
             {/* 面包屑 */}
             {/* 内容 */}
